e32ca9da38194348dbdaafdb4dfcec4e4b17500e
alper-savas <alpersavas1998@gmail.com>
2023-05-14 02:02:56 +0200

    add overlay functionality

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const slider = document.querySelector(".slider");
 const btnLeft = document.querySelector(".slider-btn-left");
 const btnRight = document.querySelector(".slider-btn-right");
 
+// overlay
+let activeElement;
+const [main] = document.getElementsByTagName("main");
+const macOverlay = document.querySelector(".mac-overlay");
+const macListItem = document.querySelector(".mac-list-item");
+const storeListItem = document.querySelector(".store-list-item");
+const storeOverlay = document.querySelector(".store-overlay");
+const ipadListItem = document.querySelector(".ipad-list-item");
+const ipadOverlay = document.querySelector(".ipad-overlay");
+
 let currentSlide = 0;
 const maxSlides = slides.length;
 
@@ -41,3 +51,25 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") prevSlide();
   if (e.key === "ArrowRight") nextSlide();
 });
+
+// activate overlay
+const activateOverlay = (overlayItem) => {
+  if (activeElement) {
+    activeElement.classList.remove("open");
+  }
+  activeElement = overlayItem;
+  activeElement.classList.add("open");
+  main.classList.add("open");
+};
+
+main.addEventListener("mouseover", () => {
+  activeElement.classList.remove("open");
+  activeElement = null;
+  main.classList.remove("open");
+});
+
+macListItem.addEventListener("mouseover", () => activateOverlay(macOverlay));
+storeListItem.addEventListener("mouseover", () =>
+  activateOverlay(storeOverlay)
+);
+ipadListItem.addEventListener("mouseover", () => activateOverlay(ipadOverlay));
